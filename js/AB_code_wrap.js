4580a85678558427609feea0c90c8676923a84a4
Andy Beckmann <abec19@gmail.com>
2020-01-07 17:34:13 -0500

    Move JS inside a function and add example code

--- js/AB_code_wrap.js
+++ js/AB_code_wrap.js
@@ -1,29 +1,37 @@
 /**
  * AB_code_wrap
  * version: 1.0.0
+ *
+ * codeWrap() wraps preformatted code blocks with a window-like UI
+ *
+ * @param {string} htmlText - Text displayed above HTML code blocks
+ * @param {string} cssText - Text displayed above CSS code blocks
+ * @param {string} javascriptText - Text displayed above JavaScript code blocks
  */
 
-var code = document.getElementsByTagName('code');
+function codeWrap (htmlText, cssText, javascriptText) {
+	var code = document.getElementsByTagName('code');
 
-for (var i=0; i<code.length; i++) {
+	for (var i=0; i<code.length; i++) {
 
-	var codeLanguage = '';
+		var codeLanguage = '';
 
-	if (code[i].classList.contains('language-javascript')) {
-		codeLanguage = 'script.js';
-	} else if (code[i].classList.contains('language-css')) {
-		codeLanguage = 'style.css';
-	} else if (code[i].classList.contains('language-html')) {
-		codeLanguage = 'index.html';
+		if (code[i].classList.contains('language-html')) {
+			codeLanguage = htmlText;
+		} else if (code[i].classList.contains('language-css')) {
+			codeLanguage = cssText;
+		} else if (code[i].classList.contains('language-javascript')) {
+			codeLanguage = javascriptText;
+		}
+
+		var pre = code[i].parentNode;
+
+		var codeToolbar = document.createElement('div');
+
+		codeToolbar.innerHTML = '<div class="code-toolbar"><div class="code-toolbar"><div class="top">' + codeLanguage + '</div><div class="code-toolbar-button red"></div><div class="code-toolbar-button yellow"></div><div class="code-toolbar-button green"></div><div class="body"></div></div></div>';
+
+		pre.parentNode.insertBefore(codeToolbar, pre);
+
+		codeToolbar.appendChild(pre);
 	}
-
-	var pre = code[i].parentNode;
-
-	var codeToolbar = document.createElement('div');
-
-	codeToolbar.innerHTML = '<div class="code-toolbar"><div class="code-toolbar"><div class="top">' + codeLanguage + '</div><div class="code-toolbar-button red"></div><div class="code-toolbar-button yellow"></div><div class="code-toolbar-button green"></div><div class="body"></div></div></div>';
-
-	pre.parentNode.insertBefore(codeToolbar, pre);
-
-	codeToolbar.appendChild(pre);
 }
